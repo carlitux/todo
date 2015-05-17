f9e744f01ffcc5c0e45c8acd2ccc504f045b2e51
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-05-17 01:24:10 -0400

    Tests for views, views and urls

--- todo/urls.py
+++ todo/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
 
-    # Examples:
-    # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls')),
 )
